0cd914df154ffe58072e1f2e7f102c9fdc7c58f4
Renan Lalier <renanc@ciandt.com>
2016-12-12 14:47:03 -0200

    Commmit inicial

--- spec/hello-world.spec.js
+++ spec/hello-world.spec.js
@@ -2,6 +2,6 @@ var calculator = require('../controllers/calculatorController')
 
 describe('Teste de Soma', function() {
     it('Soma dois valores', function() {
-        expect(calculator.sum(4, 3)).toEqual(7);
+        expect(calculator.sum(3, 3)).toEqual(7);
     });
 });
