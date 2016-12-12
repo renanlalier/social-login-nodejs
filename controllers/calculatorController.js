69184d3716cda0ee7fa06948c51de991a164775d
Renan Lalier <renanc@ciandt.com>
2016-12-12 16:17:18 -0200

    Commmit inicial

--- controllers/calculatorController.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-
-    sum: function(x, y) {
-        return x + y;
-    }
-
-}
--- spec/hello-world.spec.js
+++
@@ -1,7 +0,0 @@
-var calculator = require('../controllers/calculatorController')
-
-describe('Teste de Soma', function() {
-    it('Soma dois valores', function() {
-        expect(calculator.sum(4, 3)).toEqual(7);
-    });
-});
